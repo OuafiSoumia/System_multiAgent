b9f71dfe6ad46ceb66a337f3f5b66d7a7cd0cdd3
ExploTech <94236541+OuafiSoumia@users.noreply.github.com>
2024-12-01 14:48:23 +0000

    version final  for auto2

--- AutonomyTwo/AutonomyTwo.java
+++ AutonomyTwo/AutonomyTwo.java
@@ -5,188 +5,244 @@ import java.util.Random;
 import com.cyberbotics.webots.controller.Camera;
 import com.cyberbotics.webots.controller.CameraRecognitionObject;
 import com.cyberbotics.webots.controller.DistanceSensor;
+import com.cyberbotics.webots.controller.Emitter;
 import com.cyberbotics.webots.controller.LED;
 import com.cyberbotics.webots.controller.Motor;
+import com.cyberbotics.webots.controller.Receiver;
 import com.cyberbotics.webots.controller.Robot;
 
 public class AutonomyTwo extends Robot {
-    private int timeStep;
-    private DistanceSensor[] distanceSensor;        
-    private Motor leftMotor;
-    private Motor rightMotor;
-    private Camera camera;
-    private LED[] leds;
-    private Random random;
-    private List<String> visitedPositions; // Liste pour mémoriser les positions visitées
-    
-    public AutonomyTwo() {
-        random = new Random();
-        timeStep = 128;  // set the control time step
-        
-        // Initialisation des capteurs IR
-        distanceSensor = new DistanceSensor[8];
-        String[] sensorNames = {
-            "ps0", "ps1", "ps2", "ps3",
-            "ps4", "ps5", "ps6", "ps7"
-        };
-        for (int i = 0; i < 8; i++) {
-            distanceSensor[i] = this.getDistanceSensor(sensorNames[i]);
-            distanceSensor[i].enable(timeStep);
-        }
+	private int timeStep;
+	private DistanceSensor[] distanceSensor;	
+	private Motor leftMotor;
+	private Motor rightMotor;
+	private Camera camera;
+	private Emitter emitter;
+	private Receiver receiver;
+	private LED[] leds;
+	private Random random;
+	
+	
+	public AutonomyTwo() {
+            	random = new Random();
+		timeStep = 128;  // set the control time step
 
-        // Initialisation de la caméra
-        camera = this.getCamera("camera");
-        camera.enable(timeStep);
-        camera.recognitionEnable(timeStep);
 
-        // Initialisation des moteurs
-        leftMotor = this.getMotor("left wheel motor");
-        rightMotor = this.getMotor("right wheel motor");
-        leftMotor.setPosition(Double.POSITIVE_INFINITY);
-        rightMotor.setPosition(Double.POSITIVE_INFINITY);
-        leftMotor.setVelocity(0.0);
-        rightMotor.setVelocity(0.0);
+		// Sensors initialization 
+		// IR distance sensors
+		distanceSensor = new DistanceSensor[8];
+		String[] sensorNames = {
+				"ps0", "ps1", "ps2", "ps3",
+				"ps4", "ps5", "ps6", "ps7"
+		};
 
-        // Initialisation des LED
-        leds = new LED[10];
-        String[] ledsNames = {
-            "led0", "led1", "led2", "led3",
-            "led4", "led5", "led6", "led7",
-            "led8", "led9"
-        };
-        for (int i = 0; i < 10; i++) {
-            leds[i] = this.getLED(ledsNames[i]);
-        }
+		for (int i = 0; i < 8; i++) {
+			distanceSensor[i] = this.getDistanceSensor(sensorNames[i]);
+			distanceSensor[i].enable(timeStep);
+		}
 
-        // Initialisation de la liste des positions visitées
-        visitedPositions = new ArrayList<>();
-    }
+		// Camera
+		camera=this.getCamera("camera");
+		camera.enable(timeStep);
+		camera.recognitionEnable(timeStep);
 
-    protected double[] readDistanceSensorValues() {
-        double[] psValues = {0, 0, 0, 0, 0, 0, 0, 0};
-        for (int i = 0; i < 8 ; i++) {
-            psValues[i] = distanceSensor[i].getValue();
-        }
-        return psValues;
-    }
+		// Actuators initialization
+		// Motors
+		leftMotor = this.getMotor("left wheel motor");
+		rightMotor = this.getMotor("right wheel motor");
+		leftMotor.setPosition(Double.POSITIVE_INFINITY);
+		rightMotor.setPosition(Double.POSITIVE_INFINITY);
+		leftMotor.setVelocity(0.0);
+		rightMotor.setVelocity(0.0);
 
-    protected void move(double left, double right) {
-        double max = 6.2;
-        getMotor("left wheel motor").setVelocity(left * max / 100);
-        getMotor("right wheel motor").setVelocity(right * max / 100);
-    }
+		// LEDS
+		leds = new LED[10];
+		String[] ledsNames = {
+				"led0", "led1", "led2", "led3",
+				"led4", "led5", "led6", "led7",
+				"led8", "led9"
+		};
+		for (int i = 0; i < 10; i++) {
+			leds[i] = this.getLED(ledsNames[i]);
+		}
+	}
 
-    protected void setLED(int num, boolean on) {
-        if(num < 10) {
-            leds[num].set(on ? 1 : 0);
-        }
-    }
+	/**
+	 * 
+	 * @return a double array with values for each IR sensor 
+	 * Each value is between approx. [67 ; 750 (very close - contact)]
+	 * (see https://cyberbotics.com/doc/guide/epuck)
+	 */
+	protected double[] readDistanceSensorValues() {
+		// read sensors outputs
+		double[] psValues = {0, 0, 0, 0, 0, 0, 0, 0};
+		for (int i = 0; i < 8 ; i++){
+			psValues[i] = distanceSensor[i].getValue();
+		}
 
-    protected List<CameraRecognitionObject> cameraDetection() {
-        ArrayList<CameraRecognitionObject> detected = new ArrayList<>();
-        int nb = camera.getRecognitionNumberOfObjects();
-        if (nb > 0) {
-            CameraRecognitionObject[] objects = camera.getRecognitionObjects();
-            for (int i = 0; i < objects.length; i++) {
-                detected.add(objects[i]);
-            }
-        }
-        return detected;
-    }
+		return psValues;
+	}
 
-    protected CameraRecognitionObject targetDetected(List<CameraRecognitionObject> detected) {
-        for (CameraRecognitionObject ob : detected) {
-            if (ob.getModel().compareTo("cible") == 0)
-                return ob;
-        }
-        return null;                
-    }
+	/**
+	 * 
+	 * @param left : a value between [-100;100]%
+	 * @param right : a value between [-100;100]%
+	 */
+	protected void move(double left, double right) {
+		double max=6.2;
+		getMotor("left wheel motor").setVelocity(left * max / 100);
+		getMotor("right wheel motor").setVelocity(right * max / 100);
+	}
+	
+	/**
+	 * Switch on / off a LED according to its num ([0;9])
+	 * @param num
+	 * @param on : true if the LED is to be switched on, 
+	 * or false if the LED is to be switched off
+	 */
+	protected void setLED(int num, boolean on) {
+		if(num < 10) {
+			leds[num].set(on ? 1 : 0);
+		}
+	}
 
-    protected List<CameraRecognitionObject> otherRobotsDetected(List<CameraRecognitionObject> detected) {
-        ArrayList<CameraRecognitionObject> robots = new ArrayList<>();
-        for (CameraRecognitionObject ob : detected) {
-            if (ob.getModel().compareTo("e-puck") == 0)
-                robots.add(ob);
-        }
-        return robots;                
-    }
+	/**
+	 * 
+	 * @return an empty list if nothing is detected by the camera, 
+	 * a list of CameraRecognitionObject otherwise (see https://cyberbotics.com/doc/reference/camera#camera-recognition-object)
+	 */
+	protected List<CameraRecognitionObject> cameraDetection() {
+		ArrayList<CameraRecognitionObject> detected=new ArrayList<>();
+		int nb=camera.getRecognitionNumberOfObjects();
+		if(nb >0) {
+			CameraRecognitionObject[] objects=camera.getRecognitionObjects();
+			for(int i=0;i<objects.length;i++) {
+				detected.add(objects[i]);
+			}
+		}
+		return detected;
+	}
 
-    // Fonction pour vérifier si une position a déjà été visitée
-    protected boolean hasVisitedPosition(double x, double y) {
-        String positionKey = x + "," + y;
-        return visitedPositions.contains(positionKey);
-    }
+	/**
+	 * Look in a List of camera detected objects if the target is one of them 
+	 * @param detected: a List of camera detected objects
+	 * @return the target (a specific CameraRecognitionObject) or null
+	 */
+	protected CameraRecognitionObject targetDetected(List<CameraRecognitionObject> detected) {
+		for(CameraRecognitionObject ob:detected) {
+			if(ob.getModel().compareTo("cible") == 0)
+				return ob;
+		}
+		return null;		
+	}
 
-    // Fonction pour ajouter une position à la liste des positions visitées
-    protected void addVisitedPosition(double x, double y) {
-        String positionKey = x + "," + y;
-        if (!visitedPositions.contains(positionKey)) {
-            visitedPositions.add(positionKey);
-        }
-    }
+	/**
+	 * Look in a List of camera detected objects if other robots are recognized 
+	 * @param detected: a List of camera detected objects
+	 * @return a List of CameraRecognitionObject representing the other robots
+	 */
+	protected List<CameraRecognitionObject> otherRobotsDetected(List<CameraRecognitionObject> detected) {
+		ArrayList<CameraRecognitionObject> robots=new ArrayList<>();
+		for(CameraRecognitionObject ob:detected) {
+			if(ob.getModel().compareTo("e-puck") == 0)
+				robots.add(ob);
+		}
+		return robots;		
+	}
 
-    public void run() {        
-        int aleatoire = random.nextInt(25, 50); // Valeur aléatoire entre 25 et 50
+	/**
+	 * The main method of the robot behaviour
+	 */	
+	public void run() {
+		while (step(timeStep) != -1) {
+			double[] psValues = readDistanceSensorValues();
+			List<CameraRecognitionObject> detectedObjects = cameraDetection();
 
-        // Boucle principale de contrôle
-        while (step(timeStep) != -1) {        
-            double[] psValues = readDistanceSensorValues();
-            List<CameraRecognitionObject> detectedObjects = cameraDetection();
-            CameraRecognitionObject target = targetDetected(detectedObjects);
-            
-            // Vérification de la présence de la cible
-            if (target != null) {
-                double[] targetPosition = target.getPosition();
-                double xPosition = targetPosition[0];
-                double yPosition = targetPosition[1];
+			if (handleTargetDetection(detectedObjects)) break;
 
-                // Déplacer le robot en fonction de la position de la cible
-                if (yPosition < 0.1 && yPosition > 0) {
-                    move(-5.0, 25.0); // tourner à gauche
-                } else if (yPosition > -0.1 && yPosition <= 0) {
-                    move(25.0, -5.0); // tourner à droite
-                }
-                if (xPosition < 0.1) {
-                    move(0, 0); // Arrêter le robot si la cible est atteinte
-                    break;
-                }
-                this.step(timeStep); // Actualisation de l'état du robot
-            }
-            
-            // Gestion des obstacles
-            double threshold = 100.0;
-            if (psValues[0] < threshold && psValues[7] < threshold) {
-                move(50 + aleatoire, 50 - aleatoire); // Avancer en ligne droite
-            } else if ((psValues[2] > threshold) || (psValues[0] > threshold && psValues[1] > threshold && psValues[2] > threshold) ||
-                       (psValues[0] > threshold && psValues[1] > threshold && psValues[7] > threshold)) {
-                move(-50.0, 50.0);  // tourner à droite pour éviter l'obstacle
-            } else if ((psValues[0] > threshold && psValues[7] > threshold) ||
-                       (psValues[0] > threshold && psValues[1] > threshold && psValues[7] > threshold)) {
-                int direction = random.nextInt(2);
-                if (direction == 1) {
-                    move(-50.0, 50.0);
-                } else {
-                    move(50.0, -50.0);
-                }
-            } else if ((psValues[5] > threshold) || (psValues[5] > threshold && psValues[6] > threshold)) {
-                move(50.0, -50.0); // tourner à gauche pour éviter l'obstacle
-            } else {
-                move(-aleatoire, aleatoire);  // Déplacement aléatoire
-            }
-            
-            // Enregistrer la position du robot si elle n'a pas été visitée
-            double x = getPosition()[0];  // Récupérer la position x du robot
-            double y = getPosition()[1];  // Récupérer la position y du robot
-            
-            if (!hasVisitedPosition(x, y)) {
-                addVisitedPosition(x, y);
-            }
-        }
-    }
+			if (handleObstacleAvoidance(psValues)) continue;
 
-    public static void main(String[] args) {
-        AutonomyTwo controller = new AutonomyTwo();
-        controller.run();
-        System.out.println("============ done");
-    }
+			moveRandomly();
+		}
+	}
+
+	private boolean handleTargetDetection(List<CameraRecognitionObject> detectedObjects) {
+		CameraRecognitionObject target = targetDetected(detectedObjects);
+		if (target != null) {
+			double[] targetPosition = target.getPosition();
+			double yPosition = targetPosition[1];
+			double xPosition = targetPosition[0];
+
+			if (yPosition < 0.1 && yPosition > 0) {
+				System.out.println("x est -------------");
+				move(-5.0, 25.0); 
+				step(timeStep);
+			} else if (yPosition > -0.1 && yPosition <= 0) {
+				System.out.println("x est +++++++++");
+				move(25.0, -5.0); 
+				step(timeStep);
+			} if (xPosition < 0.1) {
+				System.out.println("else **************************");
+				move(0, 0);
+				step(timeStep);
+				return true; 
+			}
+		}
+		return false;
+	}
+
+	private boolean handleObstacleAvoidance(double[] psValues) {
+		double threshold = 100.0;
+		int aleatoire = random.nextInt(-20, 25);
+
+		if ((psValues[0] < threshold && psValues[7] < threshold)) {
+			move(50 + aleatoire, 50 - aleatoire);
+			return true;
+		}
+
+		if ((psValues[2] > threshold) ||
+			(psValues[0] > threshold && psValues[1] > threshold && psValues[2] > threshold) ||
+			(psValues[0] > threshold && psValues[1] > threshold && psValues[7] > threshold) ||
+			(psValues[0] > threshold && psValues[1] > threshold) ||
+			(psValues[0] > threshold) ||
+			(psValues[1] > threshold) ||
+			(psValues[1] > threshold && psValues[2] > threshold)) {
+			move(-50.0, 50.0);
+			return true;
+		}
+
+		if ((psValues[0] > threshold && psValues[7] > threshold) ||
+			(psValues[0] > threshold && psValues[7] > threshold && psValues[1] > threshold && psValues[6] > threshold) ||
+			(psValues[1] > threshold && psValues[7] > threshold && psValues[6] > threshold)||
+			(psValues[0] > threshold && psValues[1] > threshold && psValues[6] > threshold)) {
+			int direction = random.nextInt(2);
+			if (direction == 1) move(-50.0, 50.0);
+			else move(50.0, -50.0);
+			return true;
+		}
+
+		if ((psValues[5] > threshold) ||
+			(psValues[7] > threshold) ||
+			(psValues[5] > threshold && psValues[6] > threshold) ||
+			(psValues[6] > threshold && psValues[7] > threshold) ||
+			(psValues[5] > threshold && psValues[6] > threshold && psValues[7] > threshold) ||
+			(psValues[0] > threshold && psValues[6] > threshold && psValues[7] > threshold)) {
+			move(50.0, -50.0);
+			return true;
+		}
+
+		return false; // Aucun obstacle à éviter
+	}
+
+	private void moveRandomly() {
+		int aleatoire = random.nextInt(-20, 25);
+		move(-aleatoire, aleatoire);
+	}
+
+	public static void main(String[] args) {
+		AutonomyTwo controller = new AutonomyTwo();
+		controller.run();
+		  System.out.println("============ done");
+		  
+	}
 }
